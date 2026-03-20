953b0fcf1aa532bd0ce53c3633b922e27c46660a
Weon_hyeok2 <kwh102455@gmail.com>
2026-03-20 15:37:49 +0900

    재귀 구현도 추가함

--- 완전탐색/7568(덩치).java
+++ 완전탐색/7568(덩치).java
@@ -36,3 +36,54 @@ public class Main {
     }
 
 }
+
+
+
+
+///  재귀로 푼 형식
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Integer>[] people = new ArrayList[N];
+
+        for (int i = 0; i < people.length; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(Integer.parseInt(st.nextToken()));
+            list.add(Integer.parseInt(st.nextToken()));
+            people[i] = list;
+        }
+
+        for (int i = 0; i < N; i++) {
+            calculateRank(i, 0, 1, people);
+        }
+        System.out.print(sb.toString().trim());
+    }
+
+    static void calculateRank(int target, int current, int rankPoint, ArrayList<Integer>[] list) {
+        if (current >= list.length) {
+            sb.append(rankPoint).append(" ");
+            return;
+        }
+
+        if (list[target].get(0) < list[current].get(0) && list[target].get(1) < list[current].get(1)) {
+            ++rankPoint;
+        }
+
+        calculateRank(target, current + 1, rankPoint, list);
+    }
+
+}
+
